1235b3ac5d6a648f278bfab34ea3b300b7c00718
zhangwenyu2 <zhangwy5@chinatowercom.cn>
2017-09-17 14:34:55 +0800

    update

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,34 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloJava");
+
+        boolean a=true;
+        byte b=12;
+        char c='b';
+        short d=250;
+        int e=1000;
+        long f=20000;
+        float g=3.2f;
+        double z=5.9794;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(z);
+
+        long y=2147473648L;
+        System.out.println(y);
+
+        char c1='\377';
+        System.out.println(c1);
+        char c2='\u9fbb';
+        System.out.println(c2);
+
+        String s1=null;
+        System.out.println(s1);
     }
 }
