d46747b71034687cbf1112a4b53bc1cbf3a782ce
zhangwenyu2 <zhangwy5@chinatowercom.cn>
2017-09-24 15:55:50 +0800

    ExerciseSaturday

---
+++ src/main/java/javase/oop/Calculator.java
@@ -0,0 +1,35 @@
+package javase.oop;
+
+public class Calculator {
+    public double pi=3.1415926;
+    private int x;
+    private int y;
+
+    private boolean b;
+
+    public boolean isB() {
+        return b;
+    }
+
+    public void setB(boolean b) {
+        this.b = b;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+
+}
