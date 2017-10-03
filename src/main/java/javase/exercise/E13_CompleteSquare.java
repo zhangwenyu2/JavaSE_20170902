fda7e558014def5b3565faceca86597760cfdd17
zhangwenyu2 <zhangwy5@chinatowercom.cn>
2017-10-03 21:40:19 +0800

    E22

--- src/main/java/javase/exercise/E13_CompleteSquare.java
+++ src/main/java/javase/exercise/E13_CompleteSquare.java
@@ -2,7 +2,7 @@ package javase.exercise;
 
 public class E13_CompleteSquare {
     public static void main(String[] args) {
-        for (int i = 1;; i++) {
+        for (int i = 1;i<3000; i++) {
             double s1=Math.sqrt(i+100);
             int m=(int)s1;
             double s2=Math.sqrt(i+100+168);
