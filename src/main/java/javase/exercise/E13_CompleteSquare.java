d34d81ebd5ff683639d315112b909452a26f9b34
zhangwenyu2 <zhangwy5@chinatowercom.cn>
2017-10-03 15:42:08 +0800

    Homework_E13

---
+++ src/main/java/javase/exercise/E13_CompleteSquare.java
@@ -0,0 +1,15 @@
+package javase.exercise;
+
+public class E13_CompleteSquare {
+    public static void main(String[] args) {
+        for (int i = 1;; i++) {
+            double s1=Math.sqrt(i+100);
+            int m=(int)s1;
+            double s2=Math.sqrt(i+100+168);
+            int n=(int)s2;
+            if (((i + 100) == m * m) && ((i + 100 + 168) == n * n)) {
+                System.out.println("这个数是"+i);
+            }
+        }
+    }
+}
