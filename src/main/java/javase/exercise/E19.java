96ac2036cf0dcd55bff9b928481a0f650c7ea15d
zhangwenyu2 <zhangwy5@chinatowercom.cn>
2017-10-03 19:17:20 +0800

    E19

---
+++ src/main/java/javase/exercise/E19.java
@@ -0,0 +1,51 @@
+package javase.exercise;
+
+public class E19 {
+    public static void main(String[] args) {
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.println("x");
+
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print("x");
+        System.out.print("x");
+        System.out.println("x");
+
+        System.out.print(" ");
+        System.out.print("x");
+        System.out.print("x");
+        System.out.print("x");
+        System.out.print("x");
+        System.out.println("x");
+
+        System.out.print("x");
+        System.out.print("x");
+        System.out.print("x");
+        System.out.print("x");
+        System.out.print("x");
+        System.out.print("x");
+        System.out.println("x");
+
+        System.out.print(" ");
+        System.out.print("x");
+        System.out.print("x");
+        System.out.print("x");
+        System.out.print("x");
+        System.out.println("x");
+
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print("x");
+        System.out.print("x");
+        System.out.println("x");
+
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print("x");
+
+
+    }
+}
