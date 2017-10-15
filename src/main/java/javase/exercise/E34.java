ba062d7d990139a58fdfcf2e2e5710b4b7d04b0b
zhangwenyu2 <zhangwy5@chinatowercom.cn>
2017-10-15 17:05:05 +0800

    Abstract

--- src/main/java/javase/exercise/E34.java
+++ src/main/java/javase/exercise/E34.java
@@ -1,16 +1,14 @@
 package javase.exercise;
 
+import java.util.Random;
+
 // 随机生成[1， 20]数 10000 次，使用两种方法实现（java.lang.Math，java.util.Random）
 // 并判断两种方法的 效率 和 分布 (Hashtable)
 public class E34 {
     public static void main(String[] args) {
-        /*
-            1 - 500
-            2 - 500
-            .
-            .
-            .
-            20 -500
-         */
+        Random random=new Random();
+        for (int i = 0; i <10000 ; i++) {
+
+        }
     }
 }
