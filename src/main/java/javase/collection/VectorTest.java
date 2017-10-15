d9d92df1c839953777540be0b04421f40d1480c2
zhangwenyu2 <zhangwy5@chinatowercom.cn>
2017-10-15 16:06:01 +0800

    Abstract

---
+++ src/main/java/javase/collection/VectorTest.java
@@ -0,0 +1,37 @@
+package javase.collection;
+
+import java.util.Vector; // util - utility 实用程序\ [juːˈtɪlɪtɪ]
+
+
+public class VectorTest {
+    public static void main(String[] args) {
+        Vector<String> strings = new Vector<>(); // E Element 元素
+        strings.add("hi");
+        strings.add("hello");
+
+        System.out.println(strings.get(1));
+        System.out.println(strings.size());
+        System.out.println(strings.capacity()); // 容量,\ 能力,\ 容积
+
+        for (int i = 0; i < 2; i++) {
+            strings.add("a");
+        }
+
+//        strings.clear();
+
+//        strings.remove(0);
+
+        strings.remove("a");
+
+        strings.set(0, "test");
+
+        System.out.println(strings.size()); // 101
+        System.out.println(strings.capacity()); // ?
+
+        System.out.println("---");
+
+        for (String string : strings) {
+            System.out.println(string);
+        }
+    }
+}
